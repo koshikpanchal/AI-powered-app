41b22ddb1551758dc35be44bfd84d8748d8160d2
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-09-07 18:37:29 +0530

    setup prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,23 +1,23 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Button } from "./components/ui/button";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
-  const fetchData = async () => {
-    const { data } = await axios.get("/api/hello");
-    setMessage(data.message);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
+   const [message, setMessage] = useState('');
+   const fetchData = async () => {
+      const { data } = await axios.get('/api/hello');
+      setMessage(data.message);
+   };
+   useEffect(() => {
+      fetchData();
+   }, []);
 
-  return (
-    <div className="p-4">
-      <Button>Click me</Button>
-      <p className="font-bold text-3xl">{message}</p>
-    </div>
-  );
+   return (
+      <div className="p-4">
+         <Button>Click me</Button>
+         <p className="font-bold text-3xl">{message}</p>
+      </div>
+   );
 }
 
 export default App;
