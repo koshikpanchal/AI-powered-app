e84167f66ddb2779f70b044d7b339d252d10519b
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-09-07 17:02:59 +0530

    added shadcn

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -11,7 +12,12 @@ function App() {
     fetchData();
   }, []);
 
-  return <p className="font-bold p-4 text-3xl">{message}</p>;
+  return (
+    <div className="p-4">
+      <Button>Click me</Button>
+      <p className="font-bold text-3xl">{message}</p>
+    </div>
+  );
 }
 
 export default App;
