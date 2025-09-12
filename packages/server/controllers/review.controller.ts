64ef22d9ed26e0ad09b7bd47c5cd8fe749c2b5e3
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-09-12 10:40:23 +0530

    handleing edge casess, removing call to openai when no reviews

--- packages/server/controllers/review.controller.ts
+++ packages/server/controllers/review.controller.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from 'express';
 import { reviewService } from '../services/review.service';
+import { productRepository } from '../repositories/product.repository';
+import { reviewRespository } from '../repositories/review.repository';
 
 export const reviewController = {
    async fetchReviews(req: Request, res: Response) {
@@ -9,9 +11,23 @@ export const reviewController = {
          res.status(400).json({ error: 'Invalid Product ID' });
       }
 
-      const reviews = await reviewService.getReview(productId);
+      const product = await productRepository.getProductRepository(productId);
 
-      res.json(reviews);
+      if (!product) {
+         res.status(400).json({ error: "product doesn't exist" });
+         return;
+      }
+
+      const reviews = reviewRespository.getReviews(productId, 1);
+
+      if (!reviews) {
+         res.status(400).json({ error: "review doesn't exist" });
+         return;
+      }
+
+      const summary = await reviewService.getReview(productId);
+
+      res.json(summary);
    },
 
    async summarizeReviews(req: Request, res: Response) {
