bf4ac44306dba433eb9e504588082543ba85ce2e
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-09-15 14:18:03 +0530

    added routing using RRD

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import ChatBot from './components/chat/ChatBot';
 import ReviewList from './components/reviews/ReviewList';
 
 function App() {
    return (
       <div className="p-4 h-screen w-full">
-         {/* <ChatBot /> */}
-         <ReviewList productId={1} />
+         <Routes>
+            <Route path="/" element={<ChatBot />} />
+            <Route path="/reviews" element={<ReviewList productId={1} />} />
+         </Routes>
       </div>
    );
 }
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 const queryClient = new QueryClient();
@@ -10,7 +11,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
       <QueryClientProvider client={queryClient}>
-         <App />
+         <BrowserRouter>
+            <App />
+         </BrowserRouter>
       </QueryClientProvider>
    </StrictMode>
 );
